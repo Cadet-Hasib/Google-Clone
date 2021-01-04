3b45107e919d7de66af3ea56c5e87836b53ab6c4
Hasib Al Rashid <69360193+Cadet-Hasib@users.noreply.github.com>
2021-01-04 23:18:50 +0600

    init

--- src/API.js
+++ src/API.js
@@ -1,3 +1,3 @@
-export const key = "AIzaSyA0qfORRTxvU06jzVJ_unSZf0y3kQGZ33Q"
+export const key = "AIzaSyA0qfORRTxvU06jzVJ_unSZf0y3kQGZ33Q";
 
-export const cx = "987ea1969baeb4ab6"
+export const cx = "987ea1969baeb4ab6";
